a11a9d951c151798d3edc7db1b65b8cc060d7b8d
Arthur Uguen <98781580+artuguen28uea@users.noreply.github.com>
2022-02-07 08:37:48 -0400

    With Comments

--- ativ002/turtle_fibonacci.py
+++ ativ002/turtle_fibonacci.py
@@ -1,3 +1,5 @@
+# Python program for Plotting Fibonacci
+# spiral fractal using Turtle
 import turtle
 import math
 
@@ -8,7 +10,10 @@ def fiboPlot(n):
     square_a = a
     square_b = b
 
+    # Setting the colour of the plotting pen to blue
     x.pencolor("blue")
+
+    # Drawing the first square
     x.forward(b * factor)
     x.left(90)
     x.forward(b * factor)
@@ -17,10 +22,12 @@ def fiboPlot(n):
     x.left(90)
     x.forward(b * factor)
 
+    # Proceeding in the Fibonacci Series
     temp = square_b
     square_b = square_b + square_a
     square_a = temp
 
+    # Drawing the rest of the squares
     for i in range(1, n):
         x.backward(square_a * factor)
         x.right(90)
@@ -29,15 +36,22 @@ def fiboPlot(n):
         x.forward(square_b * factor)
         x.left(90)
         x.forward(square_b * factor)
+
+        # Proceeding in the Fibonacci Series
         temp = square_b
         square_b = square_b + square_a
         square_a = temp
 
+    # Bringing the pen to starting point of the spiral plot
     x.penup()
     x.setposition(factor, 0)
     x.seth(0)
     x.pendown()
+
+    # Setting the colour of the plotting pen to red
     x.pencolor("red")
+
+    # Fibonacci Spiral Plot
     x.left(90)
     for i in range(n):
         print(b)
@@ -51,10 +65,17 @@ def fiboPlot(n):
         b = temp + b
 
 
+# Here 'factor' signifies the multiplicative
+# factor which expands or shrinks the scale
+# of the plot by a certain factor.
 factor = 1
 
+# Taking Input for the number of
+# Iterations our Algorithm will run
 n = int(input("Enter the number of iterations (must be > 1): "))
 
+# Plotting the Fibonacci Spiral Fractal
+# and printing the corresponding Fibonacci Number
 if n > 0:
     print("Fibonacci series for", n, "elements :")
     x = turtle.Turtle()
